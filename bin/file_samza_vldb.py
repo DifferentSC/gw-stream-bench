750023a33e65abd22c5043f70d4e9704ac2cff1b
DifferentSC <bert1234@snu.ac.kr>
2018-05-26 17:39:58 +0900

    Add cache-only option

--- bin/file_samza_vldb.py
+++ bin/file_samza_vldb.py
@@ -58,7 +58,20 @@ elif args.exp_mode == "rocksdb_nvme":
     flink_command_line = flink_common_command_line + [
         "--state_backend", "rocksdb",
         "--rocksdb_path", "/nvme",
-        "--block_cache_size", str(0)
+        "--block_cache_size", str(0),
+        "--cache_enabled", "true",
+        "--cache_size", str(10000),
+        "--batch_write_size", str(1)
+    ]
+
+elif args.exp_mode == "rocksdb_nvme_cache_only":
+    flink_command_line = flink_common_command_line + [
+        "--state_backend", "rocksdb",
+        "--rocksdb_path", "/nvme",
+        "--block_cache_size", str(0),
+        "--cache_enabled", "true",
+        "--cache_size", str(10000),
+        "--batch_write_size", str(5000)
     ]
 
 elif args.exp_mode == "rocksdb_nvme_wb":
