2cf08cbdebc18b29d9eeb61c2135fdf8d03abbe1
DifferentSC <bert1234@snu.ac.kr>
2018-08-07 20:46:16 +0900

    Use concurrent skip list

--- samza-vldb-simul/src/main/java/edu/snu/splab/gwstreambench/query/SamzaVLDBExp.java
+++ samza-vldb-simul/src/main/java/edu/snu/splab/gwstreambench/query/SamzaVLDBExp.java
@@ -82,7 +82,7 @@ public class SamzaVLDBExp {
                     .setBlockSize(16 * 1024)
               )
               .setWriteBufferSize(writeBufferSize * 1024 * 1024)
-              .setMemTableConfig(new HashSkipListMemTableConfig())
+              .setMemTableConfig(new SkipListMemTableConfig())
               .setMaxWriteBufferNumber(16)
               .setTargetFileSizeBase(128 * 1024 * 1024)
               .setLevelZeroSlowdownWritesTrigger(40)
