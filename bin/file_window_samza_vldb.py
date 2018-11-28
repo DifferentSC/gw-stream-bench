e7ed3563d61d986ac739124772f2ff97787c6180
DifferentSC <bert1234@snu.ac.kr>
2018-11-28 16:09:15 +0900

    Add missing exp_mode

--- bin/file_window_samza_vldb.py
+++ bin/file_window_samza_vldb.py
@@ -5,6 +5,7 @@ import os
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument("exp_mode")
 parser.add_argument("text_file_path")
 parser.add_argument("tuple_num", type=int)
 parser.add_argument("key_num", type=int)
