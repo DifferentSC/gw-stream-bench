51ffbe458647397a1faa0c9b7aaf63977f95f4c3
Jangho Seo <jangho@jangho.io>
2019-04-14 14:41:56 +0900

    fake q12

--- nexmark-generator/src/main/java/edu/snu/splab/gwstreambench/nexmark/GeneratorMain.java
+++ nexmark-generator/src/main/java/edu/snu/splab/gwstreambench/nexmark/GeneratorMain.java
@@ -8,9 +8,12 @@ import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.core.memory.ByteArrayDataOutputView;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
-import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -18,26 +21,42 @@ public final class GeneratorMain {
     public static final void main(final String[] args) throws Exception {
         final ParameterTool params = ParameterTool.fromArgs(args);
         final int eventsPerSecond = params.getInt("events_per_sec");
+        final String kafkaBrokerAddress = params.get("kafka_broker_address");
         final TypeInformation<Event> eventTypeInfo = TypeExtractor.createTypeInfo(Event.class);
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
         env.getConfig().disableGenericTypes();
         final TypeSerializer<Event> serializer = eventTypeInfo.createSerializer(env.getConfig());
         final NexmarkSourceGenerator generator = new NexmarkSourceGenerator(eventsPerSecond);
+        final Properties props = new Properties();
+        props.put("bootstrap.servers", kafkaBrokerAddress);
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+        final Producer<String, byte[]> kafkaProducer = new KafkaProducer<>(props);
         final Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new WordPublisherRunner(eventsPerSecond, generator, serializer), 100L, 100L);
+        timer.scheduleAtFixedRate(new WordPublisherRunner(eventsPerSecond, generator, serializer, kafkaProducer),
+                100L, 100L);
     }
 
     private static final class WordPublisherRunner extends TimerTask {
+        private static final String TOPIC = "nexmarkinput";
         final int eventsToPublishInSingleRun;
         final NexmarkSourceGenerator generator;
         final TypeSerializer<Event> serializer;
         final ByteArrayDataOutputView dataOutputView = new ByteArrayDataOutputView(1000);
+        final Producer<String, byte[]> kafkaProducer;
         WordPublisherRunner(final int eventsPerSecond,
                             final NexmarkSourceGenerator generator,
-                            final TypeSerializer<Event> serializer) {
+                            final TypeSerializer<Event> serializer,
+                            final Producer<String, byte[]> kafkaProducer) {
             this.eventsToPublishInSingleRun = eventsPerSecond / 10;
             this.generator = generator;
             this.serializer = serializer;
+            this.kafkaProducer = kafkaProducer;
         }
 
         @Override
@@ -51,7 +70,7 @@ public final class GeneratorMain {
             try {
                 final Event event = generator.next();
                 serializer.serialize(event, dataOutputView);
-                System.out.println(DatatypeConverter.printHexBinary(dataOutputView.toByteArray()));
+                kafkaProducer.send(new ProducerRecord<>(TOPIC, dataOutputView.toByteArray()));
                 dataOutputView.reset();
             } catch (final IOException e) {
                 e.printStackTrace();
--- nexmark/src/main/java/edu/snu/splab/gwstreambench/nexmark/query/Query12.java
+++ nexmark/src/main/java/edu/snu/splab/gwstreambench/nexmark/query/Query12.java
@@ -1,13 +1,32 @@
 package edu.snu.splab.gwstreambench.nexmark.query;
 
+import edu.snu.splab.gwstreambench.nexmark.model.Event;
+import edu.snu.splab.gwstreambench.nexmark.model.Person;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.serialization.TypeInformationSerializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 
 import java.util.Properties;
 
 public class Query12 implements QueryBuilder {
     @Override
-    public void build(StreamExecutionEnvironment env, ParameterTool params, Properties properties) {
-
+    public void build(final StreamExecutionEnvironment env, final ParameterTool params,
+                      final Properties properties) {
+        final TypeInformation<Event> typeInformation = TypeExtractor.createTypeInfo(Event.class);
+        final DataStream<Event> events = env.addSource(
+                new FlinkKafkaConsumer011<>("nexmarkinput",
+                        new TypeInformationSerializationSchema<>(typeInformation, env.getConfig()), properties));
+        events.filter((FilterFunction<Event>) event -> event.eventType == Event.EventType.PERSON)
+                .map((MapFunction<Event, Person>) event -> event.person)
+                .map((MapFunction<Person, String>) person -> person.name)
+                .addSink(new FlinkKafkaProducer011<>("result", new SimpleStringSchema(), properties));
     }
 }
