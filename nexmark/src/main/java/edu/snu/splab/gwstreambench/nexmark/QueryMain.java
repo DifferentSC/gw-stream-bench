368946b2dce1882f15dbef28d6cab3e47218baac
Jangho Seo <jangho@jangho.io>
2019-04-22 17:02:15 +0900

    ...

--- nexmark/src/main/java/edu/snu/splab/gwstreambench/nexmark/QueryMain.java
+++ nexmark/src/main/java/edu/snu/splab/gwstreambench/nexmark/QueryMain.java
@@ -13,11 +13,14 @@ import org.apache.flink.api.common.serialization.TypeInformationSerializationSch
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.contrib.streaming.state.OptionsFactory;
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
 import org.apache.flink.runtime.state.AbstractStateBackend;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+import org.rocksdb.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -53,7 +56,57 @@ public final class QueryMain {
         // env.getConfig().disableGenericTypes();
 
         // set up state backend
-        if (!stateBackend.equals("default")) {
+        if (stateBackend.equals("rocksDB")) {
+            final String tableFormat = params.get("table_format");
+            final String dbPath = params.get("rocksdb_path", "");
+            final int writeBufferSize = params.getInt("write_buffer_size", 0);
+            final int blockCacheSize = params.getInt("block_cache_size", 0);
+            final RocksDBStateBackend rocksDBStateBackend = new RocksDBStateBackend("file:///tmp/");
+            rocksDBStateBackend.setDbStoragePath(dbPath);
+            //rocksDBStateBackend.setPredefinedOptions(PredefinedOptions.FLASH_SSD_OPTIMIZED);
+            //rocksDBStateBackend.setEnableStatistics(true);
+            rocksDBStateBackend.setOptions(new OptionsFactory() {
+                @Override
+                public DBOptions createDBOptions(DBOptions dbOptions)
+                {
+                    return dbOptions
+                            .setBytesPerSync(1024 * 1024);
+                }
+                @Override
+                public ColumnFamilyOptions createColumnOptions(ColumnFamilyOptions columnFamilyOptions) {
+
+                    final TableFormatConfig tableFormatConfig;
+                    if (tableFormat.equals("block")) {
+                        tableFormatConfig = new BlockBasedTableConfig();
+                    } else if (tableFormat.equals("plain")) {
+                        tableFormatConfig = new PlainTableConfig();
+                    } else {
+                        throw new IllegalArgumentException("RocksDB table format should be one of block or plain.");
+                    }
+
+                    return columnFamilyOptions
+                            .setTableFormatConfig(new BlockBasedTableConfig()
+                                    .setNoBlockCache(blockCacheSize == 0)
+                                    .setBlockCacheSize(blockCacheSize * 1024 * 1024)
+                                    .setBlockSize(16 * 1024)
+                            )
+                            .setWriteBufferSize(writeBufferSize * 1024 * 1024)
+                            .setMemTableConfig(new SkipListMemTableConfig())
+                            .setMaxWriteBufferNumber(1)
+                            .setTargetFileSizeBase(128 * 1024 * 1024)
+                            .setLevelZeroSlowdownWritesTrigger(40)
+                            .setLevelZeroStopWritesTrigger(46)
+                            .setBloomLocality(1)
+                            .setCompressionType(CompressionType.NO_COMPRESSION)
+                            .setTableFormatConfig(tableFormatConfig)
+                            .useFixedLengthPrefixExtractor(16)
+                            .setOptimizeFiltersForHits(false);
+                    //optimizeForPointLookup(writeBufferSize * 1024 * 1024);
+                }
+            });
+            env.setStateBackend(rocksDBStateBackend);
+
+        } else if (!stateBackend.equals("default")) {
             final StateBackendFactory stateBackendFactory = STATE_BACKENDS.get(stateBackend);
             if (stateBackendFactory == null) {
                 throw new UnsupportedOperationException(String.format("Unknown state backend: %s", stateBackend));
