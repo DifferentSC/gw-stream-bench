d29a037221bb90fc26f83b3daee4ff8c5c2af2e0
Jueun <je2156@snu.ac.kr>
2019-04-28 05:34:08 +0000

    working memory event time start-experiment script

--- bin/start-experiment-event.sh
+++ bin/start-experiment-event.sh
@@ -11,7 +11,7 @@ ssh streamix-w 'sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"'
 sleep 5
 ssh streamix-k "/home/ubuntu/flink-nvme/build-target/bin/stop-cluster.sh && /home/ubuntu/flink-nvme/build-target/bin/start-cluster.sh"
 sleep 5
-python bin/je_0418.py conf/streamix-session-window-event-time-10K.yml
+python bin/je_0418.py conf/streamix-session-window-event-time.yml
 
 
 sleep 60
