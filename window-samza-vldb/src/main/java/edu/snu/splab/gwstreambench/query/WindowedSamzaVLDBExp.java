0a1a2e399f7b30c18cfebca2e329152bc3b6b590
jueun <je2156@snu.ac.kr>
2019-05-28 10:27:59 +0900

    debugging message

--- window-samza-vldb/src/main/java/edu/snu/splab/gwstreambench/query/WindowedSamzaVLDBExp.java
+++ window-samza-vldb/src/main/java/edu/snu/splab/gwstreambench/query/WindowedSamzaVLDBExp.java
@@ -90,6 +90,7 @@ public class WindowedSamzaVLDBExp {
       return;
     }
 
+
     // get the execution environment.
     final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
     env.setParallelism(parallelism);
@@ -233,6 +234,7 @@ public class WindowedSamzaVLDBExp {
 
       if (isEventTime) {
         // parse the data, group it, window it, and aggregate the counts
+        System.out.println("system time: "+System.currentTimeMillis()+" ,timeDiff: "+timeDiff);
         count = text
           .flatMap(new FlatMapFunction<String, Tuple3<Integer, String, Long>>() {
             private final Tuple3<Integer, String, Long> result = new Tuple3<>();
